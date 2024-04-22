6d63ff0296df0dc09379483131f131944a5b9574
Narendradev7 <venkatanarendrak7@gmail.com>
2024-04-22 10:34:07 +0530

    first react app

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import Buttoncomponent from "./Components/Functionality/button/button";
+import Listcomponent from "./Components/Functionality/lists/lists";
+import TableComponent from "./Components/Functionality/tables/table";
+import FormComponent from "./Components/Functionality/forms/forms";
+
+
+const Abppl = () => {
+
+  return (
+    <div> 
+      <h1>Narendra</h1>
+      <Buttoncomponent></Buttoncomponent>
+      <Listcomponent></Listcomponent>
+      <TableComponent></TableComponent>
+      <FormComponent></FormComponent>
+    </div>
+  )
+}
+
+
+
+export default Abppl;
---
+++ my-app/src/Components/Functionality/lists/lists.js
@@ -0,0 +1,13 @@
+const Listcomponent = () => {
+
+    return (
+        <ul>
+            <li>Coffee</li>
+            <li>Tea</li>
+            <li>Milk</li>
+        </ul>  
+
+    )
+}
+
+export default Listcomponent;
